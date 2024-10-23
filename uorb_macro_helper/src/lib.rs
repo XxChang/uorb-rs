a1ba5cf52ce62c7d18b620dd3f68b09fe885335d
XxChang <xuxchang@hotmail.com>
2024-10-23 11:38:33 +0800

    add get msg hasher

--- uorb_macro_helper/src/lib.rs
+++ uorb_macro_helper/src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{DataStruct, DeriveInput, Fields, FieldsNamed};
+
+fn hash_32_fnv1a(data: &str) -> u32 {
+    let mut hash_val: u32 = 0x811c9dc5;
+    let prime: u32 = 0x1000193;
+
+    for byte in data.bytes() {
+        hash_val ^= byte as u32;
+        hash_val = hash_val.wrapping_mul(prime);
+    }
+
+    hash_val
+}
+
+pub fn get_message_hash(item: TokenStream) -> u32 {
+    let ast: DeriveInput = syn::parse2(item).unwrap();
+    match ast.data {
+        syn::Data::Struct(
+            DataStruct {
+                fields: Fields::Named(
+                    FieldsNamed {
+                        ref named,
+                        ..
+                    }
+                ),
+                ..
+            }
+        ) => {
+            let all_field_str = named.iter().fold(String::default(), |acc, ref field| {
+                let name = &field.ident;
+                let typ = &field.ty;
+                let r = quote! { #typ #name};
+                acc + &r.to_string() + "\n"
+            });
+
+            hash_32_fnv1a(&all_field_str)
+        }
+        _ => unimplemented!("Only struct named fields is supported"),
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use quote::quote;
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_message_hash() {
+        let input = quote! {
+            struct GpioIn {
+                timestamp: u64,
+                device_id: u32,
+                state: u32,
+            }
+        };
+
+        let result = get_message_hash(input);
+        assert_eq!(result, 791998605u32);
     }
 }
+
